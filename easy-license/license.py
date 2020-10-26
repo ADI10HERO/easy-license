6c56d044eab385979ea2daa913f1d256801208af
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-10-27 01:55:46 +0530

    Initialize project structure
    Signed-off-by: Aditya Srivastava <adityasrivastava301199@gmail.com>

---
+++ easy-license/license.py
@@ -0,0 +1,4 @@
+# https://docs.cryptlex.com/node-locked-licenses/using-lexactivator/using-lexactivator-with-python
+# https://app.cryptlex.com/products
+# Refer above sites next time whenever you code :)
+
